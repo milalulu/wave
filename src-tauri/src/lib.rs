3f364ab03410a122bd5770495243923c962d1f39
Wave Dev <dev@wave.local>
2026-08-14 15:40:28 -0400

    fix: tauri-plugin-global-shortcut v2 API

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -433,7 +433,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_global_shortcut::Builder::new().build())
+        .plugin(tauri_plugin_global_shortcut::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(android::init())
         .plugin(
